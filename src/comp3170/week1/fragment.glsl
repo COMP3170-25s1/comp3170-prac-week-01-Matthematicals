9f089c1e41104798a9319603c4484a7562dc259e
MatthewHo47729686 <matthew.ho2@students.mq.edu.au>
2025-03-12 16:51:45 +1100

    finished

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -7,12 +7,15 @@ layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
    vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
-  
-   if (d < 0.5) {
+   //float d = distance(p, vec2(0.5, 0.5));    // calculate distance to midpoint
+   
+   float e = abs(p.y-p.x);
+   float r = p.y+p.x;
+   if (r<1.5 && r>0.5 && e<0.5) {
       o_colour = vec4(u_colour, 1);
    }
    else {
-      o_colour = vec4(0,0,0,1); // BLACK
+      o_colour = vec4(1,1,1,1); // white
    }
+   
 }
